666fa36629bdb0854886f0d89f2120e4be5c2927
Raheem Alibhai <ral@ivoireacademie.com>
2022-08-24 13:20:06 +0800

    controller name changes and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # require 'faker'
-
-User.destroy_all
-ParkingSlot.destroy_all
 RentalRequest.destroy_all
+ParkingSlot.destroy_all
+User.destroy_all
+
+
 puts "destroyed all users, parking slots, and rental requests"
 10.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   email = Faker::Internet.email
   phone = Faker::PhoneNumber.phone_number
-  u = User.create(first_name: first_name, last_name: last_name, email: email, phone: phone)
+  password ="123456"
+  u = User.create!(first_name: first_name, last_name: last_name, email: email, phone: phone, password: password)
   puts "new user: #{u.first_name} #{u.last_name}"
 end
 all_users = User.all
@@ -27,9 +29,12 @@ all_users = User.all
   price = rand(1..10)
   title = "A great parking slot in #{Faker::Address.city}"
   description = Faker::Quotes::Shakespeare.hamlet
+  security = "Camera"
+  weather_protection = "Yes"
+  currency = "Eur"
   vt = ["car", "motorcycle", "bicycle"]
   vehicle_type = vt.sample
-  p = ParkingSlot.create(user: user, width: width, address: address, price: price, title: title, description: description, vehicle_type: vehicle_type)
+  p = ParkingSlot.create!(security: security, weather_protection: weather_protection, currency: currency, user: user, width: width, address: address, price: price, title: title, description: description, vehicle_type: vehicle_type)
   puts "new parking slot : #{p.address}"
 end
 all_parking_slots = ParkingSlot.all
@@ -38,7 +43,8 @@ all_parking_slots = ParkingSlot.all
   parking_slot = all_parking_slots.sample
   start_date = Faker::Date.birthday
   end_date = Faker::Date.forward
+  total_amount = 3
   st = "Pending"
-  r = RentalRequest.create(user: user, status: st, parking_slot: parking_slot, start_date: start_date, end_date: end_date)
+  r = RentalRequest.create!(total_amount: total_amount, user: user, status: st, parking_slot: parking_slot, start_date: start_date, end_date: end_date)
   puts "rental request start_date: #{r.start_date}, end_date: #{r.end_date}"
 end
