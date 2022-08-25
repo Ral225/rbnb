9acc32b66d60f6700c3617a4570e9ca5f1bdd7d4
Eric Williamson <ericwilli@gmail.com>
2022-08-25 15:16:05 +0800

    created the form and made it functional through the controller

--- app/controllers/parking_slots_controller.rb
+++ app/controllers/parking_slots_controller.rb
@@ -17,4 +17,22 @@ class ParkingSlotsController < ApplicationController
   def new
     @parkingslot=ParkingSlot.new
   end
+
+  def create
+    @user = current_user
+    @parkingslot = ParkingSlot.new(parkingslot_params)
+    @parkingslot.user = @user
+    if @parkingslot.save
+      redirect_to parking_slots_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def parkingslot_params
+    params.require(:parking_slot).permit(:title, :description, :vehicle_type, :height, :width, :address, :price, :security, :currency, :weather_protection)
+  end
+
 end
