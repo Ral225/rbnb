ab04151bdc931a20f1e6ce55d5d5f9a8392c7a01
Bodil Hundevad <bodil@hundevad.de>
2022-08-26 10:55:16 +0800

    card js deleted

--- app/javascript/controllers/card_controller.js
+++
@@ -1,8 +0,0 @@
-// const alert = document.querySelector("card-status");
-console.log("hello");
-
- $(".jsdata").dataset.bookings.sort(function(a,b) {
-  var c = new Date(a.created_at);
-  var d = new Date(b.created_at);
-  return c-d;
-});
